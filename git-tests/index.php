7504ae6aba463225fbe11df194cbf5f5ad703498
Christophe Lambercy <cly@cpnv.ch>
2016-09-15 23:19:16 +0200

    Ok, I made the change you requested.

--- git-tests/index.php
+++ git-tests/index.php
@@ -3,4 +3,5 @@ phpinfo();
 
 echo "Do something awesome every day", PHP_EOL;
 echo "And that awesomness might come from Git ...", PHP_EOL;
-echo "Line added by clam", PHP_EOL;
+echo "Lines added by clam", PHP_EOL;
+echo "Lines added by clam", PHP_EOL;
