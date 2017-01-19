efe08f0d383ee470a7c14e89b718e3c450622865
Christophe Lambercy <cly@cpnv.ch>
2017-01-19 22:52:20 +0100

    Version initiale

---
+++ php/login/admin.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Page admin</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+    <link rel="stylesheet" href="css/styles.css">
+  </head>
+  <body>
+    <a href="index.html">Home</a>
+    <h1>Admin</h1>
+    <p>
+      Vous ne devriez pas accéder à cette page si vous ne vous êtes pas
+      authentifié.
+    </p>
+  </body>
+</html>
---
+++ php/login/login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+    <link rel="stylesheet" href="css/styles.css">
+  </head>
+  <body>
+    <header>
+      <a href="index.html">Home</a>
+    </header>
+    <h1>Login</h1>
+    <form id="login" action="#">
+      <div>
+        <label for="username">Username: </label><!-- to remove space
+        --><input type="text" id="username">
+      </div>
+      <div>
+        <label for="password">Password: </label><!-- to remove space
+        --><input type="password" id="password">
+      </div>
+      <div>
+        <input type="submit" value="Connexion">
+      </div>
+    </form>
+  </body>
+</html>
